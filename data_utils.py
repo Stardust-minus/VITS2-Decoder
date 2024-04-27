a7c89b1808430b2b9b2d36d728f53ed71bfc76a3
Stardust·减 <2225664821@qq.com>
2024-04-27 11:13:20 +0800

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -72,9 +72,9 @@ class TextAudioSpeakerLoader(torch.utils.data.Dataset):
 
     def get_audio_text_speaker_pair(self, audiopath_sid_text):
         # separate filename, speaker_id and text
-        audiopath = audiopath_sid_text
-        print(audiopath)
-        print(len(audiopath))
+        audiopath = audiopath_sid_text[0]
+        # print(audiopath)
+        # print(len(audiopath))
         spec, wav = self.get_audio(audiopath)
         # mel_feature = torchaudio.load(mel_feature_path)
         mel_feature = get_mel_feature(audiopath)
